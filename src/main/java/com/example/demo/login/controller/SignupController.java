fc950ebefff59c4a2e75f6fafcb43d17a74247cb
久田直治郎 <naojirouhisada@gmail.com>
2019-06-12 00:24:25 +0900

    登録フォームの作成

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -4,9 +4,14 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import com.example.demo.login.domain.model.SignupForm;
+import com.example.demo.login.domain.model.GroupOrder;
 
 @Controller
 public class SignupController {
@@ -23,7 +28,7 @@ public class SignupController {
     }
 
     @GetMapping("/signup")
-    public String getSignup(Model model) {
+    public String getSignup(@ModelAttribute SignupForm form, Model model) {
 
         // ラジオボタンの初期化メソッド呼び出し
         radioMarriage = initRadioMarrige();
@@ -36,7 +41,13 @@ public class SignupController {
     }
 
     @PostMapping("/signup")
-    public String postSignup(Model model) {
+    public String postSignup(@ModelAttribute @Validated(GroupOrder.class) SignupForm form, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return getSignup(form, model);
+        }
+
+        System.out.println(form);
         // login.htmlにリダイレクト
         return "redirect:/login";
     }
